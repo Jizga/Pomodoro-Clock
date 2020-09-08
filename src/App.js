aaedc31cbef52c7b55d94566d95808f80c688ab0
Jizga <jizga93@gmail.com>
2020-09-08 13:48:08 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import Display from "./Components/Display";
+import Controller from "./Components/Controller";
+import Buttons from "./Components/Buttons";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="App-header">My Pomodoro Clock</h1>
+      <h1>My Pomodoro Clock</h1>
+      <div className="controllers">
+        <Controller />
+        <Controller />
+      </div>
+      <div className="display">
+        <Display />
+      </div>
+      <div className="buttons">
+        <Buttons />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Display.js
@@ -0,0 +1,11 @@
+const { createFactory } = require("react");
+
+import React from 'react'
+
+export default function Display() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
