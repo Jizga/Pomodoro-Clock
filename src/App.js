db4acbdc2c169d35391250cac3795a156dfcc0d1
Jizga <jizga93@gmail.com>
2020-09-08 14:46:58 +0200

    do Break element

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Controller from "./Components/Controller";
 import Display from "./Components/Display";
 import Buttons from "./Components/Buttons";
 import "./App.css";
 
 function App() {
+  const [breaks, setBreaks] = useState(5);
+  const [sessions, setSessions] = useState(25);
+
+  const increment = (e) => {
+    if (e === "breaks") {
+      setBreaks(breaks + 1);
+    }
+  };
+
+  const decrement = (e) => {
+    if (e === "breaks") {
+      setBreaks(breaks - 1);
+    }
+  };
+
   return (
     <div className="App">
       <h1>My Pomodoro Clock</h1>
       <div className="controllers">
-        <Controller />
+        <Controller
+          id="break-label"
+          title="Breaks Length"
+          size={breaks}
+          increment={() => increment("breaks")}
+          decrement={() => decrement("breaks")}
+        />
         <Controller />
       </div>
       <div className="display">
--- src/Components/Controller.js
+++ src/Components/Controller.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function Controller(props) {
-    return (
-        <div className="controller">
-            <button>UP</button>
-            <button>DOWN</button>
-        </div>
-    )
+  return (
+    <div className="controller">
+      <h2>{props.title}</h2>
+      <button onClick={props.increment}>UP</button>
+      <span>{props.size}</span>
+      <button onClick={props.decrement}>DOWN</button>
+    </div>
+  );
 }
