8904bb8dbcd774ef0371e334e1931626765cff27
Jizga <jizga93@gmail.com>
2020-09-09 16:25:26 +0200

    Added Buttons' icons

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,14 +1,21 @@
 import React from "react";
+import Play from "./Icons/Icono play.png";
+import Pause from "./Icons/pause.png";
+import Reboot from "./Icons/reboot.png";
 
 export default function Buttons(props) {
   return (
     <div className="btns">
       <div id="start_stop">
-        <button onClick={props.play}>PLAY</button>
-        <button onClick={props.pause}>PAUSE</button>
+        <button onClick={props.play}>
+          <img src={Play} alt="play" height="30" width="30" />
+        </button>
+        <button onClick={props.pause}>
+          <img src={Pause} alt="pause" height="30" width="30" />
+        </button>
       </div>
       <button id="reset" onClick={props.reboot}>
-        REBOOT
+        <img src={Reboot} alt="reboot" height="30" width="30" />
       </button>
     </div>
   );
